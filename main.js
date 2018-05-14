8e54a294a858c7cebdb7d5a790737171f4771486
zawropati <zawropati@o2.pl>
2018-05-14 12:22:48 +0200

    works

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const divs = document.querySelectorAll("#app div");
 divs.forEach(function(div){
    // div.style.transform = 'translate(50vw, 50vh)';
     div.addEventListener("click", function(){
-        div.style.transform = "translate("$(Math.random()*100), $(Math.random()*100));
-        div.style.backgroundColor = 'hsl($(Math.random()*100))';
+        div.style.transform = `translate(${Math.random()*100}vw, ${Math.random()*100}vh)`;
+        div.style.backgroundColor = `hsl(${Math.random()*360}, ${Math.random()*100}%, 50%)`;
     }
 )
 
